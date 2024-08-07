fb86c3954620dfae6e07ed98a88d04371d88dcce
Reuven Lerner <reuven@lerner.co.il>
2024-08-07 21:09:05 +0300

    gitautosync update 62

--- mymod.py
+++ mymod.py
@@ -1,4 +1,4 @@
-print('Hello from the top of mymod!')
+print(f'Hello from the top of {__name__}!')
 
 x = 100
 
@@ -7,4 +7,5 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}!'
 
-print('Hello from the bottom of mymod!')
+print(f'Hello from the bottom of {__name__}!')
+
